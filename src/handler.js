0bbfdbbed1c7f50381b3b496a715f3bfe986ff45
Mac Me <macme.tang@gmail.com>
2021-02-26 12:44:09 +0530

    update

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Your function executed successfully!'
+        message: 'Your function executed successfully.. This is just a Hello World.!'
       },
       null,
       2
