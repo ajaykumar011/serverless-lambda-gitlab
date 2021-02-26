2b35c1c08aed173bbaf48fb5d463d8ea9dbb2440
Mac Me <macme.tang@gmail.com>
2021-02-26 19:37:11 +0530

    hello

--- src/handler.js
+++ src/handler.js
@@ -1,19 +1,34 @@
-'use strict';
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+let response;
 
-module.exports.hello = async function(event) {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(
-      {
-        message: 'Your function executed successfully!',
-        params: event.queryStringParameters,
-        secret: process.env.A_VARIABLE
-      },
-      null,
-      2
-    ),
-  };
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+    try {
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
 };
+
